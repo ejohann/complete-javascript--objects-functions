29ad9b9e95e51351eac4872723b4372da629b042
Johanne <johanne_lewis@hotmail.com>
2019-07-29 11:11:44 +0100

    Inheritance using prototype function

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ var Person = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-    this.calculateAge = function(){
-        console.log(2019 - this.yearOfBirth);
-    }
 }
+
+
+Person.prototype.calculateAge =  function(){
+        console.log(2019 - this.yearOfBirth);
+    };
 
 
 var john = new Person('John', 1980, 'developer');
