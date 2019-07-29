0aa48154074ca84906a089e3cf70b3052d928438
Johanne <johanne_lewis@hotmail.com>
2019-07-29 11:15:39 +0100

    Add property to function constructor using prototype property

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ Person.prototype.calculateAge =  function(){
         console.log(2019 - this.yearOfBirth);
     };
 
+Person.prototype.lastName = 'Smith';
+
 var john = new Person('John', 1980, 'developer');
 var jane = new Person('Jane', 1997, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
@@ -17,3 +19,7 @@ var mark = new Person('Mark', 1948, 'retired');
 mark.calculateAge();
 jane.calculateAge();
 john.calculateAge();
+
+console.log(mark.lastName);
+console.log(jane.lastName);
+console.log(jane.lastName);
