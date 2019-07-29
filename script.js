145cd9a290de73c9a1bade05fd6e0fc4d6014bd1
Johanne <johanne_lewis@hotmail.com>
2019-07-29 11:37:13 +0100

    Object create

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Function constructor
-
+/*
 var Person = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
@@ -23,3 +23,30 @@ john.calculateAge();
 console.log(mark.lastName);
 console.log(jane.lastName);
 console.log(jane.lastName);
+*/
+
+
+// Object.create
+
+var personProto = {
+    calculateAge : function(){
+        console.log(2019 - this.yearOfBirth);
+     }
+}
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1980;
+john.job = 'developer';
+
+console.log(john);
+john.calculateAge();
+
+var jane = Object.create(personProto, {
+    name: {value: 'jane'},
+    yearOfBirth: {value: 1997},
+    job: {value: 'designer'}
+})
+
+console.log(jane);
+jane.calculateAge();
