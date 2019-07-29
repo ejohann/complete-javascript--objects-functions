f00104a62474afce179920d694714c58828b905c
Johanne <johanne_lewis@hotmail.com>
2019-07-29 11:13:17 +0100

    Tidy up code

--- script.js
+++ script.js
@@ -6,18 +6,14 @@ var Person = function(name, yearOfBirth, job){
     this.job = job;
 }
 
-
 Person.prototype.calculateAge =  function(){
         console.log(2019 - this.yearOfBirth);
     };
 
-
 var john = new Person('John', 1980, 'developer');
-
-john.calculateAge();
-
 var jane = new Person('Jane', 1997, 'designer');
-jane.calculateAge();
-
 var mark = new Person('Mark', 1948, 'retired');
+
 mark.calculateAge();
+jane.calculateAge();
+john.calculateAge();
