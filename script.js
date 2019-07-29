f7f082bb484be3781f6c4629c0bba469de5477af
Johanne <johanne_lewis@hotmail.com>
2019-07-29 21:16:05 +0100

    Bind method

--- script.js
+++ script.js
@@ -262,11 +262,15 @@ var emily = {
 
 john.presentation('formal', 'morning');
 
-
 john.presentation.call(emily, 'friendly', 'afternoon');
 
+//john.presentation.apply(emily, ['friendly', 'afternoon']);
 
+var johnFriendly = john.presentation.bind(john, 'friendly');
 
+johnFriendly('morning');
+
+johnFriendly('night');
 
 
 
