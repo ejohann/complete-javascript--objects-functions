182d825acc6d6846f8b8a44ae2a9f9b8c8cce2f3
Johanne <johanne_lewis@hotmail.com>
2019-07-29 11:08:54 +0100

    Create function inside function constructor

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ var Person = function(name, yearOfBirth, job){
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
+    this.calculateAge = function(){
+        console.log(2019 - this.yearOfBirth);
+    }
 }
 
 
 var john = new Person('John', 1980, 'developer');
 
-console.log(john);
+john.calculateAge();
+
+var jane = new Person('Jane', 1997, 'designer');
+jane.calculateAge();
+
+var mark = new Person('Mark', 1948, 'retired');
+mark.calculateAge();
