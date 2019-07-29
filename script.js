b82cf51870f41c355ed128b6a12aa0389c6f04f4
Johanne <johanne_lewis@hotmail.com>
2019-07-29 18:02:31 +0100

    Closures

--- script.js
+++ script.js
@@ -172,7 +172,7 @@ interviewQuestions('teacher')('Mike');
 
 
 // Immediately Invoked Function Expressions - IIFE
-
+/*
 //regular function
 function game(){
     var score = Math.random() * 10;
@@ -192,10 +192,23 @@ game();
     console.log(score >= 5 - goodLuck);
 })(5);
 
+*/
 
 
+// Closures
 
+function retirement(retirementAge){
+    var a = ' years left until retirement.';
+    return function(yearOfBirth){
+        var age = 2019 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
 
+retirement(66)(1980);
+
+var retirementUS = retirement(66);
+retirementUS(1997);
 
 
 
