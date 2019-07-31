91fe119cfa0f9d016de34d447c59eb4ad0dd84ad
Johanne <johanne_lewis@hotmail.com>
2019-07-31 10:54:56 +0100

    Lecturer's solution - basic level

--- script.js
+++ script.js
@@ -388,7 +388,7 @@ var randomQuestion = questions[randomQuestion(questions)];
       11. Display the score in the console.  HINT: user another method for this
 */  
 
-
+/*
 (function(){
     
     var Question = function(question, answers, correctAnswer){
@@ -461,3 +461,60 @@ function playGame(){
 }    
  playGame();        
 })();
+
+*/
+
+
+
+/**********************************************************************
+****  CODING CHALLENGE 7 - Lecturer Solution
+**********************************************************************/
+
+(function(){
+    
+function Question(question, answers, correct){
+    
+    this.question = question;
+    this.answers = answers;
+    this.correct = correct;
+}
+
+Question.prototype.displayQuestion = function(){
+    console.log(this.question);
+    
+    for(var i = 0; i < this.answers.length; i++){
+        console.log(i + ' : ' + this.answers[i]);
+    }
+}
+
+
+Question.prototype.checkAnswer = function(ans){
+    if(ans === this.correct){
+        console.log('Correct answer!')
+     }
+    else
+     {
+         console.log('Incorrect answer, try again!');
+     }
+}
+
+var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+var q2 = new Question('What is the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+var q3 = new Question('What best describes coding?', ['Boring', 'Hard', 'Fun', 'Tedious'], 2);
+
+var questions = [q1, q2, q3];
+
+var n = Math.floor(Math.random() * questions.length);
+
+
+questions[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer'));
+
+questions[n].checkAnswer(answer);    
+    
+    
+})();
+
+
+
